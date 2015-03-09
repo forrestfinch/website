4c1ce04efc0ddb1ddf4277466a5093305f679454
info@matthijskuiper.nl <info@matthijskuiper.nl>
2015-03-09 15:58:48 +0100

    Automated commit at 2015-03-09 14:58:47 UTC by middleman-deploy 1.0.0

---
+++ javascripts/map.js
@@ -0,0 +1,26 @@
+
+var map;
+
+function mapInit() {
+  var defactoHQ = new google.maps.LatLng(53.212124, 6.57214);
+
+  var mapOptions = {
+    zoom: 16,
+    center: defactoHQ,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    draggable: true
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var image = 'images/marker-orange.svg';
+  var defactoMarker = new google.maps.Marker({
+    position: defactoHQ,
+    clickable: false,
+    map: map,
+    icon: image
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', mapInit);
