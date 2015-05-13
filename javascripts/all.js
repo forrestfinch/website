1e978a1d5dfdf4c2cbca35607bd700789bc4c6a8
Matthijs Kuiper <info@matthijskuiper.nl>
2015-05-13 13:02:40 +0200

    Automated commit at 2015-05-13 11:02:39 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -9816,6 +9816,19 @@ function mapInit() {
 }
 
 google.maps.event.addDomListener(window, 'load', mapInit);
+$(function () {
+  $('.modal-state').on('change', function () {
+    $('body').toggleClass('modal-open', $(this).is(':checked'));
+  });
+
+  $('.modal-window').on('click', function () {
+    $(this).closest('.modal').find('.modal-state').prop('checked', false).change();
+  });
+
+  $('.modal-inner').on('click', function (event) {
+    event.stopPropagation();
+  });
+});
 
 
 
